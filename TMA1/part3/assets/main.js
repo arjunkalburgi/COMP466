5cd0c2c7d142fba2c0a14e5cff45086c7d4fac72
Arjun Kalburgi <askalburgi@gmail.com>
2017-10-10 22:35:52 -0600

    start/stop slideshow

--- TMA1/part3/assets/main.js
+++ TMA1/part3/assets/main.js
@@ -1,4 +1,4 @@
-var loaded = 0, numOfImages = 20;
+var loaded = 0, numOfImages = 20, next = true;
 
 var imagearray = new Array(numOfImages); 
 for (var i = 0; i < numOfImages; i++) {
@@ -25,17 +25,24 @@ function draw() {
 
     this._draw = function() {
 
-        console.log(myCanvas.width)
-        console.log(images[counter].width)
         ctx.clearRect(0, 0, myCanvas.width, myCanvas.height)
         ctx.drawImage(images[counter], 0, 0, images[counter].width, images[counter].height,     // source rectangle
                                        0, 0, myCanvas.width,        myCanvas.height); // destination rectangle
-        counter++; 
+        
+        if (next) {
+    	    counter++; 
+        }
         if (counter > maxNum) counter = 0;
 
         setTimeout(me._draw, 3000);
     }
-    this._draw(); //START the loop
+
+	this._draw(); //START the loop
+}
+
+function startstop() {
+	next = !next; 
+	console.log("next " + next); 
 }
 
 // code source: https://stackoverflow.com/questions/16931072/simplest-slideshow-in-html5-canvas-canvas-context-clearrect-not-working-with-se
