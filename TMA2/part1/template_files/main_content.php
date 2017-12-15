ec6c0ae5695b02758e9af6668adbbb7b29b43b09
Arjun Kalburgi <askalburgi@gmail.com>
2017-12-15 00:12:24 -0700

    Fixed a major bug lol

--- TMA2/part1/template_files/main_content.php
+++ TMA2/part1/template_files/main_content.php
@@ -1,11 +1,16 @@
 <div class="content">
     <?php
     	if (isset($_GET['content'])) {
-    		// echo "we are a go people"; 
-            include 'bookmarks/bookmark.php';
-            include 'template_files/loggedin.php';
+    		echo "we are a go people"; 
+            if(logged_in() === true){
+                include 'bookmarks/bookmark.php';
+                include 'template_files/loggedin.php';
+            } else {
+                include $_GET['content']; 
+            }
+
     	} else {
-    		// echo "we are not a go"; 
+    		echo "we are not a go"; 
 	        if(logged_in() === true){
 	            include 'template_files/loggedin.php';
 	        }else{
