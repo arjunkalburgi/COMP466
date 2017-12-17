3038e662964cb34bf289d055a65225a3f380f73a
Arjun Kalburgi <askalburgi@gmail.com>
2017-12-16 22:12:39 -0700

    update feature complete, some updates to delete as well

--- TMA2/part1/bookmarks/update_bookmarks.php
+++ TMA2/part1/bookmarks/update_bookmarks.php
@@ -3,21 +3,24 @@
 
 	// did user submit a form and was the form to update bookmarks? 
 	if (empty($_POST) === false) {
-		if ($_POST["commit"] === "Update Bookmark(s)") {
+		if ($_POST["commit"] === "Update Bookmark") {
 
-		// ensure fields are filled 
-		foreach ($_POST as $key => $value) {
-			if (empty($value)) {
-				$errors[] = 'All fields are required!';
-				break 1;
+			// ensure fields are filled 
+			foreach ($_POST as $key => $value) {
+				if (empty($value)) {
+					$errors[] = 'All fields are required!';
+					break 1;
+				}
 			}
-		}
 
-		// if all good and bookmark good, commit. 
-		if(empty($errors) === true) {
-			echo "Update Bookmark(s)"; 
-			// update_bookmarks($_POST); 
-		}
+			// if all good and bookmark good, commit. 
+			if(empty($errors) === true) {
+				// echo var_dump($_POST); 
+				if (update_bookmark($_POST)) {
+					header("Refresh:0");
+				}
+
+			}
 
 		} elseif ($_POST["commit"] === "Delete Bookmark(s)") {
 			
@@ -31,12 +34,13 @@
 
 			// if all good and bookmark good, commit. 
 			if(empty($errors) === true) {
-				// echo "Delete Bookmark(s)"; 
+				// refresh content after deletion to make sure the correct content is on the page
 				if (delete_bookmarks($_POST)) {
 					header("Refresh:0");
 				}
 			}
 
+		}
 	}
 
 ?>
@@ -46,40 +50,44 @@
 	<?php	output_editable_bookmarks();	?>
 </ul>
 
+<div id="modal1" class="modal">
+	<form class="edit-bookmark-form" action="" method="post">
+		<div class="modal-content">
+			<h4>Edit Bookmark</h4>
+			<p class="register_boxes name">
+				<input required="yes" type="text" name="bookmark_name" placeholder="Enter Bookmark Name*">
+			</p>
+			<p class="register_boxes url">
+				<input required="yes" type="text" type="url" name="bookmark_url" placeholder="Enter Bookmark URL* (E.g. https://www.google.com)">
+			</p>
+			<input type="text" name="old_name" class="old_name" value="" style="display: none;" />
+			<input type="text" name="old_url" class="old_url" value="" style="display: none;" />
+		</div>
+		<div class="modal-footer">
+			<input type="submit" name="commit" value="Update Bookmark" class="modal-action modal-close waves-effect waves-green btn-flat" >
+		</div>
+	</form>
+</div>
+
+
 <script type="text/javascript">
 	
-	function updateButtonClick(thing) {
-		console.log("update thing"); 
-		console.log(thing); 
+	function updateButtonClick(bookmark_name, bookmark_url) {
+		console.log("update bookmark_name"); 
+		console.log(bookmark_name); 
 
-		// * * * * * * * * * * * * * * * * * * * *
-		// from https://stackoverflow.com/a/133997
-		var form = document.createElement("form");
-		form.setAttribute("method", "post");
-		form.setAttribute("action", "");
+		$('#modal1 .edit-bookmark-form .register_boxes.name input').attr("value", bookmark_name); 
+		$('#modal1 .edit-bookmark-form input.old_name').attr("value", bookmark_name); 
+		$('#modal1 .edit-bookmark-form .register_boxes.url input').attr("value", bookmark_url);
+		$('#modal1 .edit-bookmark-form input.old_url').attr("value", bookmark_url); 
 
-		// bookmark field
-	    var bookmarkfield = document.createElement("input");
-		bookmarkfield.setAttribute("type", "text");
-		bookmarkfield.setAttribute("name", "bookmark_name");
-		bookmarkfield.setAttribute("value", thing);
-		form.appendChild(bookmarkfield);
+		$('#modal1').modal("open");
 
-		// commit field
-	    var commitfield = document.createElement("input");
-		commitfield.setAttribute("type", "text");
-		commitfield.setAttribute("name", "commit");
-		commitfield.setAttribute("value", "Update Bookmark(s)");
-		form.appendChild(commitfield);
-
-
-		document.body.appendChild(form);
-		form.submit();
 	}
 
-	function deleteButtonClick(thing) {
+	function deleteButtonClick(bookmark_name, bookmark_url) {
 		console.log("delete thing"); 
-		console.log(thing); 
+		console.log(bookmark_name); 
 
 		// * * * * * * * * * * * * * * * * * * * *
 		// from https://stackoverflow.com/a/133997
@@ -87,11 +95,18 @@
 		form.setAttribute("method", "post");
 		form.setAttribute("action", "");
 
-		// bookmark field
+		// bookmark name field
 	    var bookmarkfield = document.createElement("input");
 		bookmarkfield.setAttribute("type", "text");
 		bookmarkfield.setAttribute("name", "bookmark_name");
-		bookmarkfield.setAttribute("value", thing);
+		bookmarkfield.setAttribute("value", bookmark_name);
+		form.appendChild(bookmarkfield);
+
+		// bookmark url field
+	    var bookmarkfield = document.createElement("input");
+		bookmarkfield.setAttribute("type", "text");
+		bookmarkfield.setAttribute("name", "bookmark_url");
+		bookmarkfield.setAttribute("value", bookmark_url);
 		form.appendChild(bookmarkfield);
 
 		// commit field
--- TMA2/part1/database/bookmarker.php
+++ TMA2/part1/database/bookmarker.php
@@ -99,8 +99,8 @@ function output_editable_bookmarks() {
          $output[] = '<li class="collection-item">
                      <a href='.$bookmark['bookmark_url'].' target="_blank">' . $bookmark['bookmark_name'] . ' - ' .$bookmark['bookmark_url']. '</a>
                      <div class="secondary-content">
-                        <button onclick="updateButtonClick(\''.$bookmark['bookmark_name'].'\')"><i class="material-icons">edit</i></button>
-                        <button onclick="deleteButtonClick(\''.$bookmark['bookmark_name'].'\')"><i class="material-icons">delete</i></button>
+                        <button onclick="updateButtonClick(\''.$bookmark['bookmark_name'].'\', \''.$bookmark['bookmark_url'].'\')"><i class="material-icons">edit</i></button>
+                        <button onclick="updateButtonClick(\''.$bookmark['bookmark_name'].'\', \''.$bookmark['bookmark_url'].'\')"><i class="material-icons">delete</i></button>
                      </div></li>';
       }
       echo  implode('', $output);
@@ -110,71 +110,28 @@ function output_editable_bookmarks() {
 
 }
 
-/*
- * This function, validates the update_bookmark page, to ensure that the user is doing the right thing while modifying
- * returns a true if validation was successful, and false if otherwise
- */
-function validate_bookmark_update($post_array){
+function update_bookmark($post_array) {
 
-   $validate = true;
-   if(empty($post_array) === false) {
-      array_pop($post_array); //remove the last element, because it contains the submit buttons data
+   $result = true; 
 
-      //first ensure that all required fields on the form has been filled
-      foreach ($post_array as $key => $value) {
-         //if any of the submitted values from the $post_array are empty, then break
-         if (empty($value[0]) || empty($value[1])) {  //here, the post array values is also an assoc array
-            $GLOBALS['errors'][] = 'All fields are required!';
-            $validate = false;
-            break 1;    //even if we find one
-         }
+   if (count($post_array) > 0) {
 
-         if($validate === true){
-            //checking the entered bookmark urls
-            if(check_url($value[1]) === false){
-               $validate = false;
-               $GLOBALS['errors'][] = 'A URL entered, is invalid!';
-               break 1;
-            }
-         }
-      }
+      $user_id = $_SESSION['user_id']; 
+      $bookmark_name = sanitize($post_array["bookmark_name"]); 
+      $bookmark_url = sanitize($post_array["bookmark_url"]); 
+      $old_name = sanitize($post_array["old_name"]);
+      $old_url = sanitize($post_array["old_url"]);
 
-   }
-
-   return $validate;
-}
-
-function update_selected_bookmarks($post_array, $old_data_array){
-
-   $update_result = false;
-   if(empty($post_array) === false && empty($old_data_array) === false) {
-      array_pop($post_array); //remove the last element, because it contains the submit buttons data
-
-      $sql_string = '';
-      $index_old_data = 0;
-      $user_id = $GLOBALS['session_user_id'];
-
-
-      //we know that the the position of element in the post_array, corresponds with the position of element in the old_data_array
-      foreach ($post_array as $key => $value) {
-
-         $old_name = key($old_data_array[$index_old_data]);
-         $old_url = $old_data_array[$index_old_data][$old_name];
-
-         $sql_string .= "UPDATE bookmarks SET bookmark_name='$value[0]', bookmark_url='$value[1]' WHERE user_id_ref='$user_id' AND bookmark_name='$old_name' AND bookmark_url='$old_url';";
-
-         $index_old_data++;
-      }
+      $sql_string = "UPDATE bookmarks SET bookmark_name='$bookmark_name', bookmark_url='$bookmark_url' WHERE user_id_ref='$user_id' AND bookmark_name='$old_name' AND bookmark_url='$old_url';";
 
       if (mysqli_multi_query($GLOBALS['connect'], $sql_string)) {
-         $update_result = true;
+         $create_result = true;
       } else {
-         $update_result = false;
+         $GLOBALS['errors'][] = "Error: Could not commit the action, please try again.";
       }
-
    }
-   return $update_result;
 
+   return $result;
 }
 
 function delete_bookmarks($post_array) {
@@ -185,15 +142,14 @@ function delete_bookmarks($post_array) {
 
       $user_id = $_SESSION['user_id']; 
       $bookmark_name = sanitize($post_array["bookmark_name"]); 
+      $bookmark_url = sanitize($post_array["bookmark_url"]);
 
-
-      $sql_string = "DELETE FROM bookmarks WHERE user_id_ref='$user_id' AND bookmark_name='$bookmark_name';";
-
+      $sql_string = "DELETE FROM bookmarks WHERE user_id_ref='$user_id' AND bookmark_name='$bookmark_name' AND bookmark_url='$bookmark_url';";
 
       if (mysqli_multi_query($GLOBALS['connect'], $sql_string)) {
          $create_result = true;
       } else {
-         $GLOBALS['errors'][] = "Error: Could not commit the bookmark, please try again.";
+         $GLOBALS['errors'][] = "Error: Could not commit the action, please try again.";
       }
    }
 
